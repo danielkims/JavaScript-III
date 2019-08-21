dc3625167f3cbcc3ed3632db519a403107a83b01
Daniel <danielkim.s@outlook.com>
2019-08-21 14:43:58 -0700

    Finished This.js Assignment

--- assignments/this.js
+++ assignments/this.js
@@ -16,16 +16,37 @@ function greeting(name) {
     console.log("Hello " + name);
     console.log(this);
 };
-greeting(Danny);
+greeting("Danny");
 
 // Principle 2
 
 // code example for Implicit Binding
+const dotObj = {
+    greet: "Hi",
+    sayHi: function(name) {
+        console.log(`${this.greet}, is your name ${name}?`);
+    }
+};
+dotObj.sayHi("Will");
 
 // Principle 3
 
 // code example for New Binding
+function greets(name) {
+    this.greets = "Hiya ";
+    this.name = name;
+    this.announce = function() {
+        console.log(this.greets + this.name);
+    }
+};
+const Eunice = new greets('Eunice');
+const John = new greets("John");
+
+Eunice.announce();
+John.announce();
 
 // Principle 4
 
 // code example for Explicit Binding
+Eunice.announce.call(John);
+John.announce.apply(Eunice);
